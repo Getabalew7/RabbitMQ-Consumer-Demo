879f48329d39317e2017a765220efb955ac49d47
getabalew7 <getabalew7@gmail.com>
2023-07-14 00:41:25 +0200

    Rabbit MQ Message consumer project setup

---
+++ src/main/java/com/codeaz/rabbitmqconsumerdemo/RabbitMqConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codeaz.rabbitmqconsumerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqConsumerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqConsumerDemoApplication.class, args);
+    }
+
+}
